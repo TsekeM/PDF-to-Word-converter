f66b83a093ab669b50658e9221bba768cc31e999
Tseke Maila <47187842+TsekeM@users.noreply.github.com>
2023-02-27 14:54:17 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import pdf2docx
+
+# path of the PDF file
+pdf_file_path = 'path/to/file.pdf'
+
+# path to save the output Word file
+output_path = 'example.docx'
+
+# convert PDF to Word
+pdf2docx.parse(pdf_file_path, output_path)
